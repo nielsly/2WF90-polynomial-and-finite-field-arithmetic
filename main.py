5e39518771d7c09b9511e4e90127b6a2018be224
Bas van den Boom <info@cloudcake.net>
2020-10-14 13:50:38 +0200

    Added back answers in input file + answer validation for display_poly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import asn1tools as asn
 import json
+
+import asn1tools as asn
+
+from Poly import Poly
 
 ### STUDENT PERSPECTIVE (example) ###
 
@@ -11,34 +14,44 @@ ops_loc = base_location + 'operations.asn'
 exs_loc = base_location + 'input.ops'
 
 # Compile specification
-spec = asn.compile_files(ops_loc, codec = "jer")
+spec = asn.compile_files(ops_loc, codec="jer")
 
 # Read exercise list
-exercise_file = open(exs_loc, 'rb') # open binary file
-file_data = exercise_file.read() # read byte array
-my_exercises = spec.decode('Exercises', file_data) # decode after specification
-exercise_file.close() # close file
+exercise_file = open(exs_loc, 'rb')  # open binary file
+file_data = exercise_file.read()  # read byte array
+my_exercises = spec.decode('Exercises', file_data)  # decode after specification
+exercise_file.close()  # close file
 
 # Create answer JSON
 my_answers = {'exercises': []}
 
 # Loop over exercises and solve
 for exercise in my_exercises['exercises']:
-    operation = exercise[0] # get operation type
-    params = exercise[1] # get parameters
-    
+    operation = exercise[0]  # get operation type
+    params = exercise[1]  # get parameters
+
+    if operation == 'display-poly':
+        poly = Poly(params['f'])
+        own_answer = str(poly)
+
+        print("{} :".format(exercise))
+        print("Correct: {} - Own answer: [{}] - Correct answer: [{}]\n"
+              .format(own_answer == params['answer'], own_answer, params['answer']))
+        params['answer'] = 'X+3'
+        params['answer-poly'] = [1, 3]
+
     if operation == 'add-field':
         params['answer'] = 'X+3'
-        params['answer-poly'] = [1,3]
-    
+        params['answer-poly'] = [1, 3]
+
     if operation == 'add-table':
         params['answer'] = ['X+1', '2X+1']
-        params['answer-poly'] = [[1,1], [2,1]]
+        params['answer-poly'] = [[1, 1], [2, 1]]
 
     # Save answer
-    my_answers['exercises'].append({operation : params})
+    my_answers['exercises'].append({operation: params})
 
 # Save exercises with answers to file
-my_file = open(base_location + "output.ops", "wb+") # write to binary file
-my_file.write(json.dumps(my_answers).encode()) # add encoded exercise list
+my_file = open(base_location + "output.ops", "wb+")  # write to binary file
+my_file.write(json.dumps(my_answers).encode())  # add encoded exercise list
 my_file.close()

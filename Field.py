7929ced27b115e8ee676c51f854f7e3d3f213be6
L-bytes <62182894+L-bytes@users.noreply.github.com>
2020-10-18 14:52:26 +0200

    Update Field.py

--- Field.py
+++ Field.py
@@ -20,7 +20,7 @@ class Field:
         return (f - g) % self.poly
     
     def multiply(self, f: Poly, g:Poly):
-        return f * g % self.poly
+        return (f * g) % self.poly
 
     def equals(self, f: Poly, g: Poly):
         return (f % self.poly) == (g % self.poly)
